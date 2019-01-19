a5fee4e34c9723bb0a6732286abfa3938d710dde
yukimura1227 <takamura1227@gmail.com>
2019-01-19 21:25:54 +0900

    feat: チケット登録のときにユーザとのヒモ付情報を登録できように実装

--- app/admin/tickets.rb
+++ app/admin/tickets.rb
@@ -42,6 +42,8 @@ index do
       f.input :b_name
       f.input :b_mail
       f.input :comment
+      # NOTE: emailをnameに変更することでnameを表示できるが、nameが現状必須項目でない かつ nameがない場合は選択肢に表示できないので、emailを出すようにしています。nameにする場合は、User.nameが必須で入るようにvalidationを入れる必要があります。
+      f.input :user_id, as: :select, collection: User.pluck(:email, :id).to_h
     end
     f.actions
   end
