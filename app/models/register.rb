7ccfccf0518fa828bd22816fd4191f6424195dea
yukimura1227 <takamura1227@gmail.com>
2019-04-07 17:38:45 +0900

    feat: add default value for registers.state

--- app/models/register.rb
+++ app/models/register.rb
@@ -1,3 +1,4 @@
 class Register < ApplicationRecord
   belongs_to :user
+  enum state: { init: 0, confirmed: 10 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190309090335) do
+ActiveRecord::Schema.define(version: 20190407083724) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20190309090335) do
     t.integer "stage_id"
     t.integer "type_id"
     t.text "comment"
-    t.integer "state"
+    t.integer "state", default: 0
     t.integer "ticket_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
