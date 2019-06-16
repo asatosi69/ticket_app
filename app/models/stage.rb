0f2de377af369f175972ef4abdbd18577339e3d1
yukimura1227 <takamura1227@gmail.com>
2019-06-16 09:57:21 +0900

    feat: add validation

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -17,4 +17,13 @@ class Stage < ApplicationRecord
     validates :performance, presence: true #この行を追加しました(2019/1/22)
     validates :total, numericality: { greater_than_or_equal_to: 0 } #この行を追加しました(2019/1/22)
     validates :deadline, presence: true #この行を追加しました(2019/1/22)
+
+  before_destroy :validate_ticket_presence
+
+  private
+
+  def validate_ticket_presence
+    errors.add :base, '紐づくチケットが残っているため、削除できません'
+    throw(:abort) if tickets.blank?
+  end
 end
