6ec0ab275ccf718c7c274000036f62ce959936a5
Ubuntu <ubuntu@ip-172-31-87-224.ec2.internal>
2019-07-17 10:23:31 +0000

    2019-07-17

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  ActiveAdmin.routes(self) rescue ActiveAdmin::DatabaseHitDuringLoad
   get 'welcome/index'
   root to: "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
