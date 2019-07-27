f4181bc37882c9a05c3e153733cda9557da5fdc0
yukimura1227 <takamura1227@gmail.com>
2019-07-27 22:04:04 +0900

    feat: only allows same user id

--- app/controllers/ticket_summary_controller.rb
+++ app/controllers/ticket_summary_controller.rb
@@ -1,7 +1,10 @@
 # For Ticket Summary
 class TicketSummaryController < ApplicationController
   before_action :authenticate_user!
+
+  # /users/1/ticket_summary.json
   def user_summary
+    fail if params[:id].to_i != current_user.id
     @user = User.find(params[:id])
   end
 end
