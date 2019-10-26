db02774df528817e950c7ebc21effe260a6afa9c
yukimura1227 <takamura1227@gmail.com>
2019-10-26 21:45:23 +0900

    feat: manage links page

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class LinksController < ApplicationController
+  def create
+    Link.new(type_id: params[:type_id], stage_id: params[:stage_id]).save
+  end
+
+  def destroy
+    Link.find(params[:id]).destroy
+  end
+
+  private
+end
