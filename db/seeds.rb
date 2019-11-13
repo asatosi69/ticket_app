c81b6ee009cece1d345f48eec3be619c4e4836b5
yukimura1227 <takamura1227@gmail.com>
2019-11-13 18:34:01 +0900

    fix: colorsのレコードがseedの都度追加されないように改善

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 #   coding: utf-8
 
-Color.create!(color_code: '#FF0000', color_name: '赤')
-Color.create!(color_code: '#00FF00', color_name: '黄緑')
-Color.create!(color_code: '#0000FF', color_name: '青')
-Color.create!(color_code: '#FFFF00', color_name: '黄')
-Color.create!(color_code: '#00FFFF', color_name: '水色')
-Color.create!(color_code: '#FF00FF', color_name: 'ピンク')
-Color.create!(color_code: '#008000', color_name: '緑')
-Color.create!(color_code: '#000080', color_name: '群青')
-Color.create!(color_code: '#808000', color_name: 'オリーブ')
-Color.create!(color_code: '#800080', color_name: '紫')
-Color.create!(color_code: '#800000', color_name: '栗色')
+Color.destroy_all
+Color.create!(id: 1, color_code: '#FF0000', color_name: '赤')
+Color.create!(id: 2, color_code: '#00FF00', color_name: '黄緑')
+Color.create!(id: 3, color_code: '#0000FF', color_name: '青')
+Color.create!(id: 4, color_code: '#FFFF00', color_name: '黄')
+Color.create!(id: 5, color_code: '#00FFFF', color_name: '水色')
+Color.create!(id: 6, color_code: '#FF00FF', color_name: 'ピンク')
+Color.create!(id: 7, color_code: '#008000', color_name: '緑')
+Color.create!(id: 8, color_code: '#000080', color_name: '群青')
+Color.create!(id: 9, color_code: '#808000', color_name: 'オリーブ')
+Color.create!(id: 10, color_code: '#800080', color_name: '紫')
+Color.create!(id: 11, color_code: '#800000', color_name: '栗色')
 
 [
   { name: '当日精算', discount_rate: 0 },
