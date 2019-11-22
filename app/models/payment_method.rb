2b8dd24fd828755cbbda587bf5feae53f04c0037
yukimura1227 <takamura1227@gmail.com>
2019-11-22 12:18:36 +0900

    chore: nicknameは3文字で

--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -1,4 +1,4 @@
 class PaymentMethod < ApplicationRecord
-  validates :nickname, presence: true
+  validates :nickname, length: { is: 3 }
   validates :discount_rate, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
