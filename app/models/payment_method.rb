40c1a5031b7c6dac877d8ef1de1e58c7927183a5
yukimura1227 <takamura1227@gmail.com>
2019-11-22 12:22:25 +0900

    feat: 全角ひらがな・カタカナ・漢字チェックを実装

--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -1,4 +1,5 @@
 class PaymentMethod < ApplicationRecord
   validates :nickname, length: { is: 3 }
+  validates :nickname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角ひらがな・カタカナ・漢字のみで入力してください' }
   validates :discount_rate, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
