cf2b80379f94d7163f6b9fe81e7d2dae49ad988c
yukimura1227 <takamura1227@gmail.com>
2019-11-22 11:24:59 +0900

    feat: 省略名を必須に

--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -1,3 +1,4 @@
 class PaymentMethod < ApplicationRecord
+  validates :nickname, presence: true
   validates :discount_rate, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
